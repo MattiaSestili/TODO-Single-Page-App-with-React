8c902faf307b63e3ec64ced31891693e30d8567e
MattiaSestili <mattia.sestili@gmail.com>
2020-02-20 08:07:57 +0000

    Add files via upload

---
+++ webpack.dev.ts
@@ -0,0 +1,25 @@
+import * as webpack from "webpack";
+import * as HtmlWebPackPlugin from "html-webpack-plugin";
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html"
+});
+
+const config: webpack.Configuration = {
+  mode: "development",
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: [".ts", ".tsx", ".js", ".json"]
+  },
+  devtool: "eval-source-map",
+
+  module: {
+    rules: [
+      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+      { test: /\.tsx?$/, loader: "awesome-typescript-loader" }
+    ]
+  },
+  plugins: [htmlPlugin]
+};
+
+export default config;
